17aa904158d08eeeb6fb34b59dc165630e35c8c4
Mark Wayne Pulido <markwayne@MacBook-Pro-3.local>
2018-10-30 17:37:48 +0800

    Updated manage.py

--- manage.py
+++ manage.py
@@ -6,3 +6,12 @@ for x in range(2000, 3200):
             results = results + str(x) + ","
 
 print(results)
+
+results = ""
+for x in range(2000, 3200):
+    if x % 7 == 0 and y % 5 != 0:
+            results = results + str(x) + ","
+
+print(results)
+
+nums = [i for i in range(2000, 3200) if (i % 7) == 0 and (i % 5) != 0]
