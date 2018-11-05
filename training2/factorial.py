7416e9f1418b3d84235463be767fed9fb751cf60
Mark Wayne Pulido <markwayne@MacBook-Pro-3.local>
2018-11-05 13:46:27 +0800

    Updated factorial

--- training2/factorial.py
+++ training2/factorial.py
@@ -1,5 +1,17 @@
 given_number = 8
-holder = 1
-for x in range(1, given_number + 1):
-    holder = holder * x
-print(holder)
+# holder = 1
+# for x in range(1, given_number + 1):
+#     holder = holder * x
+# print(holder)
+
+results = [x for x in range(1, given_number + 1)]
+
+
+def multiply_list_function(my_list):
+    holder = 1
+    for x in my_list:
+        holder *= x
+    return holder
+
+
+print(multiply_list_function(results))
